3e8256d1b4962ad96924c13ad0923ecbd1ec0749
evanebb <git@evanus.nl>
2025-03-18 16:28:31 +0100

    Rework database schema to support teams + better data model for API-driven app

--- resources/database/seeds/00001_users.sql
+++ resources/database/seeds/00001_users.sql
@@ -1,16 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
 TRUNCATE users CASCADE;
-INSERT INTO users (uuid, username, firstname, lastname, role)
+INSERT INTO users (uuid, username, role)
 VALUES ('4f42bc8c-4c92-4fe7-b2d6-31a425128870',
         'adminuser',
-        'Admin',
-        'User',
         'admin'),
        ('1a74e377-33f3-416f-88da-98fc16add21e',
         'normaluser',
-        'Normal',
-        'User',
         'user');
 
 INSERT INTO local_auth_users (uuid, username, password_hash)
@@ -22,6 +18,10 @@ VALUES ('4f42bc8c-4c92-4fe7-b2d6-31a425128870',
         'normaluser',
            -- Welkom02!
         '$2y$12$rSVIBbxHKnfQnzeaJFfoouZuBeiyjSrxPyZIz/6L2CnrwScTYWJiq');
+
+INSERT INTO namespaces (uuid, name, user_uuid)
+VALUES ('4a9e18f4-64aa-4972-a586-581f31504594', 'adminuser', '4f42bc8c-4c92-4fe7-b2d6-31a425128870'),
+       ('4970ce16-ceb2-4436-920b-e335fde10abe', 'normaluser', '1a74e377-33f3-416f-88da-98fc16add21e');
 -- +goose StatementEnd
 
 -- +goose Down
--- resources/database/seeds/00002_repositories.sql
+++ resources/database/seeds/00002_repositories.sql
@@ -1,27 +1,23 @@
 -- +goose Up
 -- +goose StatementBegin
 TRUNCATE repositories;
-INSERT INTO repositories (uuid, namespace, name, visibility, owner_uuid)
+INSERT INTO repositories (uuid, namespace, name, visibility)
 VALUES ('51151395-897d-4b9b-822b-a0c8796178cf',
-        'adminuser',
+        '4a9e18f4-64aa-4972-a586-581f31504594',
         'public-image',
-        'public',
-        '4f42bc8c-4c92-4fe7-b2d6-31a425128870'),
+        'public'),
        ('0c4e0de5-14b8-4719-9ccd-4bc73f742c65',
-        'adminuser',
+        '4a9e18f4-64aa-4972-a586-581f31504594',
         'private-image',
-        'private',
-        '4f42bc8c-4c92-4fe7-b2d6-31a425128870'),
+        'private'),
        ('db5bda5a-e7b9-4bde-91b9-fe3bcd687f63',
-        'normaluser',
+        '4970ce16-ceb2-4436-920b-e335fde10abe',
         'public-image',
-        'public',
-        '1a74e377-33f3-416f-88da-98fc16add21e'),
+        'public'),
        ('c89f0494-f9f8-419c-bc58-07949afcc7f1',
-        'normaluser',
+        '4970ce16-ceb2-4436-920b-e335fde10abe',
         'private-image',
-        'private',
-        '1a74e377-33f3-416f-88da-98fc16add21e');
+        'private');
 -- +goose StatementEnd
 
 -- +goose Down
