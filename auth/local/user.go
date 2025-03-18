f7835992f41a60e3fa9fb8e1055edcf8de81aac5
evanebb <git@evanus.nl>
2025-03-18 16:53:33 +0100

    Add convenience method to check password for auth user

--- auth/local/user.go
+++ auth/local/user.go
@@ -1,9 +1,16 @@
 package local
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type AuthUser struct {
 	ID           uuid.UUID
 	Username     string
 	PasswordHash []byte
+}
+
+func (u AuthUser) CheckPassword(password string) error {
+	return bcrypt.CompareHashAndPassword(u.PasswordHash, []byte(password))
 }
