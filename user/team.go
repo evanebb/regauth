bcaefb7500e71f46962ada9d2454c28cb29521a8
evanebb <git@evanus.nl>
2025-03-24 12:12:37 +0100

    Add validation for team name

--- user/team.go
+++ user/team.go
@@ -3,11 +3,41 @@ package user
 import (
 	"errors"
 	"github.com/google/uuid"
+	"regexp"
 )
 
 type Team struct {
 	ID   uuid.UUID `json:"id"`
-	Name string    `json:"name"`
+	Name TeamName  `json:"name"`
+}
+
+func (t Team) IsValid() error {
+	if err := t.Name.IsValid(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type TeamName string
+
+var validTeamName = regexp.MustCompile(`^[a-zA-Z0-9-_]+$`)
+
+func (n TeamName) IsValid() error {
+	l := len([]rune(n))
+	if l < 2 {
+		return errors.New("team name cannot be shorter than 2 characters")
+	}
+
+	if l > 255 {
+		return errors.New("team name cannot be longer than 255 characters")
+	}
+
+	if !validTeamName.MatchString(string(n)) {
+		return errors.New(`team name can only contain alphanumeric characters, "-" and "_"`)
+	}
+
+	return nil
 }
 
 type TeamMember struct {
